429daed10103a650f853b53fed83bdf582dac258
thesmahmud77 <mahmudsazzad77@gmail.com>
2025-10-31 13:06:14 -0700

    add money updtae

--- Java/home.js
+++ Java/home.js
@@ -1,5 +1,26 @@
-document.getElementById("add-money").addEventListener("click", function (e) {
-  e.preventDefault();
-  const availableBalance = getElementById("available-balance");
-  console.log(availableBalance);
-});
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const bankName = document.getElementById("bank").value;
+    const accountNumber = parseInt(
+      document.getElementById("account-number").value
+    );
+    const cashInAmt = document.getElementById("add-amount").value;
+    const pinNumber = document.getElementById("add-pin").value;
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+
+    // console.log(
+    //   bankName,
+    //   accountNumber,
+    //   cashInAmt,
+    //   pinNumber,
+    //   availableBalance
+    // );
+
+    const newBalance = availableBalance + cashInAmt;
+    document.getElementById("available-balance").innerText = newBalance;
+  });
